020f6b7770a391cf69e8d72c83fae5e088c4051f
Sindre Stephansen <sindre@sindrestephansen.com>
2017-02-20 17:41:59 +0100

    Added pages the rest of the pages necessary for resetting passwords

--- buk/urls.py
+++ buk/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_done, name='password_reset_complete'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
