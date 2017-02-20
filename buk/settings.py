f75d4ffe8ea4d261e38ba0992d84e5f4911ad612
Sindre Stephansen <sindre@sindrestephansen.com>
2017-02-20 13:35:30 +0100

    Added a page for resetting passwords, and cleaned up the stylesheets.

--- buk/settings.py
+++ buk/settings.py
@@ -26,13 +26,13 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members',
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
