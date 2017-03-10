d03246bcbe9d8769665350fe5879df8c25cbd8d5
Sindre Stephansen <sindre@sindrestephansen.com>
2017-03-10 19:41:07 +0100

    Removed 'instrument' as a searchable field on the useradmin, as it caused some errors.

--- members/admin.py
+++ members/admin.py
@@ -23,7 +23,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'is_active', 'get_full_name', 'instrument', 'phone',
             'get_full_address', 'has_car', 'has_towbar')
     list_filter = ('is_active', 'instrument', 'groups')
-    search_fields = ('email', 'instrument', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
     ordering = ('-is_active', 'instrument', 'first_name', 'last_name')
     filter_horizontal = ('groups',)
 
