0392b30c69d8ad817dd0a64f08c0fa2cc54d0323
Sindre Stephansen <sindre@sindrestephansen.com>
2017-03-13 22:40:34 +0100

    Added a base for time periods

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Period(models.Model):
+    start = models.DateField('start')
+    end = models.DateField('slutt', null=True)
+
+    class Meta:
+        abstract = True
+        verbose_name = 'periode'
+        verbose_name_plural = 'perioder'
+        ordering = ['end', 'start']
+
+    def contains(self, date):
+        return date >= self.start and (not self.end or date < self.end)
+
+    def __str__(self):
+        result = '%s - ' % self.start
+        if self.end:
+            result += str(self.end)
+        return result
