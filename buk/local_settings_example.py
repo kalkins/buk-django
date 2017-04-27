5984b111d0981ce1b8515251f213718605903f6c
Sindre Stephansen <sindre@sindrestephansen.com>
2017-04-27 23:13:21 +0200

    Update the local_settings example to be more plug-and-play

--- buk/local_settings_example.py
+++ buk/local_settings_example.py
@@ -8,17 +8,42 @@ like translation be committed to version control and shared, while stuff like
 database setup is placed here.
 """
 
+import sys
+
 # SECURITY WARNING: keep the secret key used in production secret!
-# You can generate your own key here:
-# http://www.miniwebtool.com/django-secret-key-generator/
 SECRET_KEY = 'd6w(v!yjnk2osjnz+xl*9tdlt##_^()z*u8pwn9^uee4442#*o'
+
+ALLOWED_HOSTS = [
+    '*',
+]
+
+INTERNAL_IPS = ['localhost', '127.0.0.1']
+
+MEDIA_ROOT = 'media-root/'
+
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buk',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+    },
 }
+
+"""Uncomment these lines to be able to import from legacy database
+if 'test' not in sys.argv:
+    DATABASES['legacy'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'buk',
+        'USER': 'buk',
+        'PASSWORD': 'buk',
+        'HOST': '',
+    }
+"""
