58e43664acc0e4692471a022c6439b926fc8a79a
Sindre Stephansen <sindre@sindrestephansen.com>
2018-02-06 18:51:00 +0100

    Fix forum URL

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ from .views import PostCreate, PostUpdate, PostDetail, PostList
 
 urlpatterns = [
     url(r'^$', PostList.as_view(), name='forum_post_list'),
-    url(r'^(?P<forum>[a-zA-Z]+)$', PostList.as_view(), name='forum_post_list'),
+    url(r'^(?P<forum>[a-zA-Z]+)/$', PostList.as_view(), name='forum_post_list'),
     url(r'^(?P<forum>[a-zA-Z]+)/ny', PostCreate.as_view(), name='forum_post_create'),
     url(r'^(?P<forum>[a-zA-Z]+)/(?P<pk>[0-9]+)$', PostDetail.as_view(),
         name='forum_post_detail'),
