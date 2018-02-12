edb98d1510e246e52ea028bbb334038a9d01193b
Sindre Stephansen <sindre@sindrestephansen.com>
2018-02-12 22:26:00 +0100

    Add tests for PollAnswerForm

--- polls/tests.py
+++ polls/tests.py
@@ -9,6 +9,7 @@ from members.models import Instrument
 from members.tests import generate_member
 
 from .models import Poll, PollOption
+from .forms import PollAnswerForm
 
 
 class PollTestCase(TestCase):
@@ -90,3 +91,102 @@ class PollOptionTestCase(TestCase):
 
         with self.assertRaisesMessage(ObjectDoesNotExist, 'matching query does not exist'):
             PollOption.objects.get(pk=option.pk)
+
+
+class PollAnswerFormTestCase(TestCase):
+    def setUp(self):
+        self.member1 = generate_member()
+        self.member2 = generate_member()
+        self.poll1 = Poll.objects.create(title='Poll 1')
+        self.poll2 = Poll.objects.create(title='Poll 1')
+        self.option1 = PollOption.objects.create(title='Option 1', poll=self.poll1)
+        self.option2 = PollOption.objects.create(title='Option 2', poll=self.poll1)
+        self.option3 = PollOption.objects.create(title='Option 3', poll=self.poll2)
+        self.option4 = PollOption.objects.create(title='Option 4', poll=self.poll2)
+
+    def test_queryset(self):
+        form1 = PollAnswerForm(poll=self.poll1, member=self.member1)
+        form2 = PollAnswerForm(poll=self.poll2, member=self.member1)
+
+        self.assertEqual(list(form1.fields['options'].queryset.all()), [self.option1, self.option2])
+        self.assertEqual(list(form2.fields['options'].queryset.all()), [self.option3, self.option4])
+
+    def test_create_without_poll(self):
+        with self.assertRaisesMessage(ValueError, 'poll'):
+            PollAnswerForm(member=self.member1)
+
+    def test_create_without_member(self):
+        with self.assertRaisesMessage(ValueError, 'member'):
+            PollAnswerForm(poll=self.poll1)
+
+    def test_deadline(self):
+        form = PollAnswerForm(poll=self.poll1, member=self.member1)
+        self.assertFalse(form.fields['options'].disabled)
+
+        self.poll1.deadline = timezone.now() - timedelta(minutes=5)
+        self.poll1.save()
+        form = PollAnswerForm(poll=self.poll1, member=self.member1)
+        self.assertTrue(form.fields['options'].disabled)
+
+    def test_initial(self):
+        form = PollAnswerForm(poll=self.poll1, member=self.member1)
+        self.assertIsNone(form.fields['options'].initial)
+
+        self.option1.members.add(self.member1)
+        form = PollAnswerForm(poll=self.poll1, member=self.member1)
+        self.assertEqual(form.fields['options'].initial, self.option1)
+
+    def test_validation(self):
+        form = PollAnswerForm(
+            poll=self.poll1,
+            member=self.member1,
+            data={
+                'options': self.option1.pk,
+            }
+        )
+        self.assertNotIn('options', form.errors)
+
+        form = PollAnswerForm(
+            poll=self.poll1,
+            member=self.member1,
+            data={}
+        )
+        self.assertIn('options', form.errors)
+
+        form = PollAnswerForm(
+            poll=self.poll1,
+            member=self.member1,
+            data={
+                'options': None,
+            }
+        )
+        self.assertIn('options', form.errors)
+
+        self.poll1.deadline = timezone.now() + timedelta(minutes=5)
+        form = PollAnswerForm(
+            poll=self.poll1,
+            member=self.member1,
+            data={
+                'options': self.option1.pk,
+            }
+        )
+        self.assertNotIn('options', form.errors)
+
+        self.poll2.deadline = timezone.now() - timedelta(minutes=5)
+        form = PollAnswerForm(
+            poll=self.poll2,
+            member=self.member2,
+            data={
+                'options': self.option3.pk,
+            }
+        )
+        self.assertIn('options', form.errors)
+
+        form = PollAnswerForm(
+            poll=self.poll1,
+            member=self.member1,
+            data={
+                'options': self.option3.pk,
+            }
+        )
+        self.assertIn('options', form.errors)
