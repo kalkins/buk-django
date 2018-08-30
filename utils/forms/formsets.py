7f217f4f475264a6fc7d33b14e1b2cd557929de7
Simen Holmestad <simen@holmestad.com>
2018-08-30 21:38:30 +0200

    Improved form-to-POST conversion

--- utils/forms/formsets.py
+++ utils/forms/formsets.py
@@ -1,3 +1,18 @@
+def form_to_post(form):
+    """Extract POST data from a form"""
+    post = {}
+
+    for field in form.fields:
+        name = form[field].html_name
+        value = form[field].value()
+        if not value and value != 0:
+            value = ""
+
+        post[name] = value
+
+    return post
+
+
 def formset_to_post(formset):
     """
     Extract POST data that will recreate the formset.
@@ -6,10 +21,6 @@ def formset_to_post(formset):
     forms = [formset.management_form, *formset.forms]
 
     for form in forms:
-        for field in form.fields:
-            name = form[field].html_name
-            value = form[field].value()
-
-            post[name] = value
+        post.update(form_to_post(form))
 
     return post
